5fcccd34fc56764d2648a6c1e7006275a9ffb086
BobyWoby <bestdogeever865@gmail.com>
2024-10-06 13:24:14 -0500

    initial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/OpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/OpenCVTest.java
@@ -1,36 +1,17 @@
 package org.firstinspires.ftc.teamcode.auto;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import androidx.annotation.NonNull;
-
-// RR-specific imports
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
-
-// Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 
 @Config
 @Autonomous(name = "BLUE_TEST", group = "Autonomous")
-public class BlueTestAuto extends LinearOpMode {
+public class OpenCVTest extends LinearOpMode {
     private OpenCvWebcam webcam;
     @Override
     public void runOpMode() throws InterruptedException {

94f426bf2736b3e99b3aa9eaec5be5d963193f3a
BobyWoby <bestdogeever865@gmail.com>
2024-10-06 14:35:07 -0500

    added locating processor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueTestAuto.java
@@ -39,31 +39,31 @@ public class BlueTestAuto extends LinearOpMode {
         // instantiate MecanumDrive at starting position
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(180)));
 
-//        Claw claw = new Claw(hardwareMap);
+        Claw claw = new Claw(hardwareMap);
 
-//        Lift lift = new Lift(hardwareMap);
+        Lift lift = new Lift(hardwareMap);
 
-//        Action trajectory1;
+        Action trajectory1;
         // actionBuilder builds from the drive steps passed to it,
         // and .build(); is needed to build the trajectory
-//        trajectory1 = drive.actionBuilder(drive.pose)
-//                .lineToYSplineHeading(33, Math.toRadians(0))
-//                .waitSeconds(2)
-//                .setTangent(Math.toRadians(90))
-//                .lineToY(48)
-//                .setTangent(Math.toRadians(0))
-//                .lineToX(32)
-//                .strafeTo(new Vector2d(44.5, 30))
-//                .turn(Math.toRadians(180))
-//                .lineToX(47.5)
-//                .waitSeconds(3)
-//                .build();
-//
-//        Action trajectory2 = drive.actionBuilder(drive.pose)
-//                .build();
-//
-//        Action trajectory3 = drive.actionBuilder(drive.pose)
-//                .build();
+        trajectory1 = drive.actionBuilder(drive.pose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2)
+                .setTangent(Math.toRadians(90))
+                .lineToY(48)
+                .setTangent(Math.toRadians(0))
+                .lineToX(32)
+                .strafeTo(new Vector2d(44.5, 30))
+                .turn(Math.toRadians(180))
+                .lineToX(47.5)
+                .waitSeconds(3)
+                .build();
+
+        Action trajectory2 = drive.actionBuilder(drive.pose)
+                .build();
+
+        Action trajectory3 = drive.actionBuilder(drive.pose)
+                .build();
 
         while(!isStopRequested() && !opModeIsActive()){
             telemetry.addData("Position during Init", drive.pose.toString());
@@ -72,14 +72,14 @@ public class BlueTestAuto extends LinearOpMode {
         waitForStart();
         if(isStopRequested()) return;
 
-//        Actions.runBlocking(
-//                new SequentialAction(
-//                        trajectory1
-//                        lift.liftUp(),
-//                        claw.openClaw(),
-//                        lift.liftDown(),
-//                        trajectory3
-//                )
-//        );
+        Actions.runBlocking(
+                new SequentialAction(
+                        trajectory1,
+                        lift.liftUp(),
+                        claw.openClaw(),
+                        lift.liftDown(),
+                        trajectory3
+                )
+        );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/OpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/OpenCVTest.java
@@ -1,66 +1,70 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import android.annotation.SuppressLint;
+import android.util.Size;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.opencv.ColorRange;
+import org.firstinspires.ftc.vision.opencv.ColorSpace;
+import org.firstinspires.ftc.vision.opencv.ImageRegion;
+import org.opencv.core.RotatedRect;
+import org.opencv.core.Scalar;
 import org.openftc.easyopencv.OpenCvWebcam;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+
+import java.util.List;
 
 
 @Config
-@Autonomous(name = "BLUE_TEST", group = "Autonomous")
+@Autonomous(name = "OPENCV", group = "Autonomous")
 public class OpenCVTest extends LinearOpMode {
     private OpenCvWebcam webcam;
+    @SuppressLint("DefaultLocale")
     @Override
     public void runOpMode() throws InterruptedException {
-//       f
+        //Blue HSV Mask Processor
+        ColorBlobLocatorProcessor colorLocator = new ColorBlobLocatorProcessor.Builder()
+                .setTargetColorRange(new ColorRange(ColorSpace.HSV, new Scalar(80, 90, 90), new Scalar(160, 255, 255)))
+                .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-1, 1, 1, -1))
+                .setDrawContours(true)
+                .setErodeSize(5)
+                .setDilateSize(5)
+                .build();
+        VisionPortal portal = new VisionPortal.Builder()
+                .addProcessor(colorLocator)
+                .setCameraResolution(new Size(320, 240))
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .build();
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
 
         // instantiate MecanumDrive at starting position
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(180)));
-
-//        Claw claw = new Claw(hardwareMap);
-
-//        Lift lift = new Lift(hardwareMap);
-
-//        Action trajectory1;
-        // actionBuilder builds from the drive steps passed to it,
-        // and .build(); is needed to build the trajectory
-//        trajectory1 = drive.actionBuilder(drive.pose)
-//                .lineToYSplineHeading(33, Math.toRadians(0))
-//                .waitSeconds(2)
-//                .setTangent(Math.toRadians(90))
-//                .lineToY(48)
-//                .setTangent(Math.toRadians(0))
-//                .lineToX(32)
-//                .strafeTo(new Vector2d(44.5, 30))
-//                .turn(Math.toRadians(180))
-//                .lineToX(47.5)
-//                .waitSeconds(3)
-//                .build();
-//
-//        Action trajectory2 = drive.actionBuilder(drive.pose)
-//                .build();
-//
-//        Action trajectory3 = drive.actionBuilder(drive.pose)
-//                .build();
-
+//        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(180)));
+        while(opModeInInit() && !opModeIsActive()){
+            List<ColorBlobLocatorProcessor.Blob> blobs = colorLocator.getBlobs();
+//            ColorBlobLocatorProcessor.Util.filterByArea();
+            telemetry.addLine(" Area Density Aspect Center");
+            for(ColorBlobLocatorProcessor.Blob b : blobs){
+                RotatedRect boxFit = b.getBoxFit();
+                telemetry.addLine(String.format("%5d  %4.2f   %5.2f  (%3d,%3d)",
+                        b.getContourArea(), b.getDensity(), b.getAspectRatio(), (int) boxFit.center.x, (int) boxFit.center.y));
+            }
+            telemetry.update();
+        }
         while(!isStopRequested() && !opModeIsActive()){
-            telemetry.addData("Position during Init", drive.pose.toString());
+//            telemetry.addData("Position during Init", drive.pose.toString());
             telemetry.update();
         }
         waitForStart();
         if(isStopRequested()) return;
 
-//        Actions.runBlocking(
-//                new SequentialAction(
-//                        trajectory1
-//                        lift.liftUp(),
-//                        claw.openClaw(),
-//                        lift.liftDown(),
-//                        trajectory3
-//                )
-//        );
     }
 }
